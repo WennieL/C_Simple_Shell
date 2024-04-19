a48bb9487f2908ea25b10c2413cc0949dbb59caf
WennieL <jecxu@hotmail.com>
2024-04-19 07:10:19 +0000

    read_line file

---
+++ shell.c
@@ -0,0 +1,17 @@
+#ifdef SHELL_H
+#define SHELL_H
+
+/*---Libraries---*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/* shell_interactive.c */
+char *read_line(void);
+
+
+#endif /*--SHELL_H--*/
+
+
