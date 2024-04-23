0acae6547e17038f1ca698b82f5f9e4fa229dcad
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-23 11:02:21 +1000

    Create get_line.c

---
+++ get_line.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+/**
+ * _getline - reads a line of input from stdin
+ *
+ * Return: a pointer to the allocated buffer containing the input line
+ *         or NULL if an error occurs or if end of file is reached
+ */
+
