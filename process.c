1095b849c0671b8e94dae2587039abd4496c4476
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-23 10:42:16 +1000

    Update process.c

--- process.c
+++ process.c
@@ -1,30 +1,26 @@
 #include <shell.h>
 
-int process(char **args)
+int execute_command(char **args)
 {
 	pid_t pid;
 	int status;
 
 	pid = fork();
-	if (pid == 0)
+	if (pid == -1)
+	{
+		perror("Error in processL forking");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
 	{
 		if (execvp(args[0], args) == -1)
 		{
 			perror("Error in process: Child process");
+			exit(EXIT_FAILURE);
 		}
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-	{
-		perror("Error in process: forking");
 	}
 	else
 	{
-		do
-		{
-			waitpid(pid, &status, WUNTRACED);
-		}
-		while (!WIFEXITED(status) && !WIFSIGNAL(status));
+		waitpid(pid, &status, 0);
 	}
-	return (-1);
 }
