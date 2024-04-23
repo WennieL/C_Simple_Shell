89be8013814c308589bba4038bed0cd178f2ceb6
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-23 10:43:23 +1000

    Create execute_command.c

---
+++ execute_command.c
@@ -0,0 +1,26 @@
+#include <shell.h>
+
+int execute_command(char **args)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("Error in processL forking");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("Error in process: Child process");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+}
