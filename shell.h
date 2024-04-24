2382b9f22903c14ca9c0ca44a2cc82e9b50dbf15
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-24 12:56:42 +1000

    Update shell.h

--- shell.h
+++ shell.h
@@ -5,13 +5,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 /* shell_interactive.c */
-char *get_line(void);
+char *get_line(interactive);
 char **split_line(char *line);
 int execute_command(char **args);
+void enviro(void);
+char **argv_arr(char *input);
 
 
 #endif /*--SHELL_H--*/
