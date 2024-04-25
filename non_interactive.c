bc6015cf9716c2859ce3be87fdafd3eb145c6ed6
WennieL <109007964+WennieL@users.noreply.github.com>
2024-04-26 09:39:43 +1000

    Update non_interactive.c

--- non_interactive.c
+++ non_interactive.c
@@ -1,9 +1,19 @@
 #include "shell.h"
 
 /**
- * non_interactive:
+ * non_interactive - Run the shell in non-interactive mode
  *
+ * This function runs the shell in non-interactive mode, where it reads
+ * commands from a stream (e.g., from a file or another program's output)
+ * rather than directly from user input. It continuously reads lines from
+ * the stream, splits them into arguments, and executes the commands or
+ * built-in functions using the execute_args function. It continues this
+ * process until the end of the stream is reached or until a termination
+ * condition is met.
  *
+ * Return: This function does not return any value directly. However, it
+ * may exit the program with a status code based on the execution result
+ * of the commands or built-in functions.
  */
 
 void non_interactive(void)
